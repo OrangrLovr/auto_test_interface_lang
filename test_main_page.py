1f0f6ba2e1dab20d3ae866c34cf3acbe3915d25b
Resker <goldgobra@gmail.com>
2022-05-13 12:38:06 +0200

    added verification of entry, registration and the main entry link

--- test_main_page.py
+++ test_main_page.py
@@ -1,16 +1,44 @@
+import pytest
 from .pages.main_page import MainPage
+from .pages.login_page import LoginPage
 
-
+@pytest.mark.skip(reason="Test successful")
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
     page.open()
     page.go_to_login_page()
 
+@pytest.mark.skip(reason="Test successful")
 def test_guest_should_see_login_link(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
 
+def test_guest_should_be_login_url(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+
+    form = LoginPage(browser, link)
+    form.should_be_login_url()
     
+def test_guest_should_be_login_form(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+
+    form = LoginPage(browser, link)
+    form.should_be_login_form()
+
+def test_guest_should_be_register_form(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+
+    form = LoginPage(browser, link)
+    form.should_be_register_form()
